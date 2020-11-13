d55d36848804830d28986c111ef366c80ca133e7
yash-93 <yashdeepbachhas93@gmail.com>
2020-11-14 02:12:29 +0530

    Portfolio

---
+++ portfolio/precache-manifest.ed757ace6d576d769d2d5d6a2f392d42.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a65d1e5da31f96356b89a5bd51efbdc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "099e8794e23eaed3b970",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "d8eee77cf0e22a4cb906",
+    "url": "/static/css/main.f4b4f50a.chunk.css"
+  },
+  {
+    "revision": "099e8794e23eaed3b970",
+    "url": "/static/js/2.4453e76f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.4453e76f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8eee77cf0e22a4cb906",
+    "url": "/static/js/main.51080b33.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "f7f12d8b700f6d8f96ca0c84a7a366e3",
+    "url": "/static/media/dev3.f7f12d8b.svg"
+  }
+]);
